51db5ec85b10b0b9b960247e3fcd0d6fe3e929d9
m-honarzade <honarzadeh.mahdie@gmail.com>
2023-10-16 16:22:21 +0330

    replace useState with useReducer

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,45 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { step: 1, count: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "countSet":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("the action is not valid");
+  }
+};
 
 const Counter = () => {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
   const currDate = new Date();
   currDate.setDate(currDate.getDate() + count);
 
   const clickHandler = () => {
-    setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
+  const incHandler = () => {
+    dispatch({ type: "inc", payload: 1 });
+  };
+  const decHandler = () => {
+    dispatch({ type: "dec", payload: -1 });
+  };
+  const countHandler = (e) => {
+    dispatch({ type: "countSet", payload: +e.target.value });
+  };
+  const stepHandler = (e) => {
+    dispatch({ type: "setStep", payload: +e.target.value });
+  };
   return (
     <div className="container flex flex-col mx-auto max-w-2xl text-center mt-16 justify-center items-center ">
       <div className="flex items-center justify-center mmx-auto mb-1 border-violet-400   text-violet-900  w-[50%]">
@@ -19,7 +48,7 @@ const Counter = () => {
           min="1"
           max="10"
           value={step}
-          onChange={(e) => setStep(+e.target.value)}
+          onChange={stepHandler}
           className="bg-violet-900"
         />
         <span className="text-violet-700 text-sm ml-2 font-semibold">
@@ -28,7 +57,7 @@ const Counter = () => {
       </div>
       <div className=" mx-auto  border-violet-400 rounded-md border bg-violet-900 text-white py-4 w-[50%]">
         <button
-          onClick={() => setCount((curr) => curr - step)}
+          onClick={decHandler}
           className="mr-1 border rounded-md px-2 py-[0.5] bg-white font-bold text-violet-800"
         >
           -
@@ -37,10 +66,10 @@ const Counter = () => {
           value={count}
           type="text"
           className="rounded-sm text-violet-900 px-2"
-          onChange={(e) => setCount(+e.target.value)}
+          onChange={countHandler}
         />
         <button
-          onClick={() => setCount((curr) => curr + step)}
+          onClick={incHandler}
           className="ml-1 border rounded-md px-2 py-[0.5] bg-white font-bold text-violet-800"
         >
           +
